0b527530a28f22abe6954a593c4d2bf8cc455c35
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-07-15 21:50:04 +0300

    ь

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,15 +3,33 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+// 1. Создаем рендерную разметку
 
+const galleryEl = document.querySelector('.gallery');
 
-`<div class="gallery__item">
-<a class="gallery__link" href="large-image.jpg">
-  <img
-    class="gallery__image"
-    src="small-image.jpg"
-    data-source="large-image.jpg"
-    alt="Image description"
-  />
-</a>
-</div>`
+const createGalleryCardsMarkup = gallery => gallery.map(
+    ({ preview, original, description }) => 
+    `<div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg"> 
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`
+).join('');
+
+const galleryItemsCards = createGalleryCardsMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryItemsCards);
+//console.log(galleryItemsCards)
+
+// gallery__link навесить 
+/*
+1 нужно сделать розметку по массиву
+2 при рендери галереи нужно поставить в розметку правильные поля
+3 повесить клик на галерею используя делегирование
+4 вызначиты елемент на який кликнули через evtyt.target
+5 о считати дани с атрибута
